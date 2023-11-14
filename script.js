6e9d81119dd199980d8c3cab9ed3e11cee5bca0b
SarahMit <sarah.mittenentzwei@ovgu.de>
2023-11-14 20:49:54 +0100

    some additional game ideas

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Load CSV file
-    fetch("gameideas.csv")
+    fetch("https://sarahmit.github.io/gamejamideagenerator/gameideas.csv")
         .then(response => response.text())
         .then(csv => {
             // Parse CSV
@@ -11,24 +11,38 @@ document.addEventListener("DOMContentLoaded", function () {
             const data = [];
             for (let i = 1; i < rows.length; i++) {
                 const values = rows[i].split(';');
-                const entry = {};
-                headers.forEach((header, index) => {
-                    entry[header.trim()] = values[index].trim();
-                });
-                data.push(entry);
+
+                // Check if the number of values matches the number of headers
+                if (values.length === headers.length) {
+                    const entry = {};
+                    headers.forEach((header, index) => {
+                        entry[header.trim()] = values[index].trim();
+                    });
+                    data.push(entry);
+                } else {
+                    console.warn(`Skipping row ${i + 1} because the number of values does not match the number of headers.`);
+                }
             }
 
             // Store the data and headers globally
             window.csvData = data;
             window.csvHeaders = headers;
 
-            // Populate the columns dropdown
-            const columnsSelect = document.getElementById('columns');
+            // Populate the columns checkbox
+            const checkboxContainer = document.getElementById('checkbox-container');
             headers.forEach(header => {
-                const option = document.createElement('option');
-                option.value = header;
-                option.text = header;
-                columnsSelect.appendChild(option);
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.id = header;
+                checkbox.checked = true; // Initially, all checkboxes are checked
+                checkbox.addEventListener('change', generateRandom);
+                
+                const label = document.createElement('label');
+                label.htmlFor = header;
+                label.appendChild(document.createTextNode(header));
+
+                checkboxContainer.appendChild(checkbox);
+                checkboxContainer.appendChild(label);
             });
         })
         .catch(error => console.error("Error fetching CSV:", error));
@@ -36,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function generateRandom() {
     // Get selected columns
-    const selectedColumns = Array.from(document.getElementById('columns').selectedOptions).map(option => option.value);
+    const selectedColumns = Array.from(document.querySelectorAll('#checkbox-container input:checked')).map(checkbox => checkbox.id);
 
     if (selectedColumns.length === 0) {
         alert("Please select at least one column.");
